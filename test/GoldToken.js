4077906809b5dbd9d66c3cc391cd428542b7ca41
prolingua <prolingua.geo@gmail.com>
2022-11-12 16:29:03 +0700

    Update GoldToken.js

--- test/GoldToken.js
+++ test/GoldToken.js
@@ -1,5 +1,6 @@
 const {
     expectEvent,
+    expectRevert,
     BN
   } = require('@openzeppelin/test-helpers');
 const web3 = require('web3');
@@ -9,7 +10,7 @@ const etherToWei = (n) => {
 const TOTAL_SUPPLY = etherToWei(1000_000);
 const GoldToken = artifacts.require('GoldToken');
 
-contract('GoldToken', async ([owner, otherAccount]) => {
+contract('GoldToken', async ([owner, otherAccount, otherAccount2]) => {
     beforeEach(async () => {
         this.goldToken = await GoldToken.new(TOTAL_SUPPLY);
     });
@@ -19,7 +20,9 @@ contract('GoldToken', async ([owner, otherAccount]) => {
             const totalSupply = await this.goldToken.totalSupply();
             expect(totalSupply.toString()).to.equal(TOTAL_SUPPLY.toString());
         });
+    });
 
+    describe("Events", () => {
         it("Should emit Transfer event with correct arguments", async () => {
             const trx = await this.goldToken.transfer(otherAccount, 1000, {from:owner});
             expectEvent(trx, "Transfer", {
@@ -27,6 +30,13 @@ contract('GoldToken', async ([owner, otherAccount]) => {
                 to: otherAccount,
                 value: new BN('1000')
             });
+        });
+    });
+
+    describe("Reverts", () => {
+        it("Should cause revert 'ERC20: transfer amount exceeds balance'", async () => {
+            await expectRevert(this.goldToken.transfer(otherAccount2, 1000, {from: otherAccount}),
+            "ERC20: transfer amount exceeds balance")
         });
     });
 });
